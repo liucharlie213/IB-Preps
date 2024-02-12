9b34a31655c1518a305463c0ca7abd33cd55675b
liucharlie213 <liu.charlie213@gmail.com>
2024-02-11 22:18:04 -0500

    added logic for my subjects page

--- client/src/components/Subjects/Subjects.js
+++ client/src/components/Subjects/Subjects.js
@@ -12,11 +12,32 @@ function Subjects() {
   ]
   const subjectsToShow = subjects.slice(0, 3);
 
-  //dynamic data to grab from database
-  const subjectContent = [
-    "Numbers and Algebra",
-    "Functions"
+  const [activeContent, setActiveContent] = useState(false);
+  // dynamic data to grab from database
+  const contentTopics = [
+    "Algebra",
+    "Calculus"
   ]
+
+  const [activeExam, setActiveExam] = useState(false);
+  // dynamic data to grab from database
+  const examSet = [
+    "Mock Exam 1",
+    "Mock Exam 2"
+  ]
+
+  const handleClickContent = () => {
+    setActiveContent(current => !current);
+    setActiveExam(false);
+    // method to grab data from database and set the contentTopics array
+  }
+
+  const handleClickExam = () => {
+    setActiveExam(current => !current);
+    setActiveContent(false);
+    // method to grab data from database and set the examSet array
+  }
+
 
   return (
     <Layout>
@@ -36,7 +57,7 @@ function Subjects() {
       </div>
     
       <div className="subjects__modes">
-        <div className="subjects__mode">
+        <div onClick={handleClickContent} className="subjects__mode" id="subjects__mode__content">
           {/* <img src={}></img> */}
           <p>Content</p>
         </div> 
@@ -44,13 +65,44 @@ function Subjects() {
           {/* <img src={}></img> */}
           <p>Exploration</p>  
         </div>
-        <div className="subjects__mode">
+        <div onClick={handleClickExam} className="subjects__mode" id="subjects__mode__exams">
           {/* <img src={}></img> */}
           <p>Mock Exams</p>
         </div>
       </div>
       {/* <div className="subejcts__milestones"></div> */}
     </div>
+
+    {activeContent && (
+      <div className="subjects__container">
+        <div className="subjects__content__topics">
+          {contentTopics.map((topic, index) => (
+            <div key={index} className="subjects__content__topic">
+              <div className="subjects__content__topic__progress">
+                {topic}
+                {/* <span></span> */}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    )}
+
+    {activeExam && (
+      <div className="subjects__container">
+        <div className="subjects__exams__mocks">
+         {examSet.map((topic, index) => (
+           <div key={index} className="subjects__exams__mock">
+            <div className="subjects__exams__topic__progress">
+              {topic}
+              {/* <span className="subjects__content__topic__progress"></span> */}
+            </div>
+          </div>
+         ))}
+        </div>
+      </div>
+       
+    )}
     </Layout>
   )
 }
